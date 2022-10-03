372a27f444ea10d5961e891d347c71036418f5e7
REDA LAGHRISSI <laghrissi.laghrissi36@gmail.com>
2022-10-03 15:31:33 +0100

    Patient class and Agent class modified

--- test-app/src/main/java/org/cnss/AgentManagement.java
+++ test-app/src/main/java/org/cnss/AgentManagement.java
@@ -1,5 +1,44 @@
 package org.cnss;
 
 public class AgentManagement {
+    private String email;
+    private String password;
+    private boolean verified;
 
+
+    //    *************************************************************** constructors *********************
+    public AgentManagement(){}
+    public AgentManagement(String email, String password, boolean verified) {
+        this.email = email;
+        this.password = password;
+        this.verified = verified;
+    }
+
+
+    //    *************************************************************** getters *********************
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+
+    //    *************************************************************** setters *********************
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
 }
--- test-app/src/main/java/org/cnss/Patient.java
+++ test-app/src/main/java/org/cnss/Patient.java
@@ -1,4 +1,54 @@
 package org.cnss;
 
 public class Patient {
+    private int matricule;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+//    *************************************************************** constructors *********************
+    public Patient(){}
+    public Patient(int matricule, String firstName, String lastName, String email) {
+        this.matricule = matricule;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+    //    *************************************************************** getters *********************
+    public int getMatricule() {
+        return matricule;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    //    *************************************************************** setters *********************
+    public void setMatricule(int matricule) {
+        this.matricule = matricule;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
