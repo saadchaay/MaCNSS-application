b3d45aefb1c128aed11284301977248e87c33c11
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-10-03 09:15:51 +0100

    update all

---
+++ test-app/src/main/java/org/cnss/Database.java
@@ -0,0 +1,51 @@
+package org.cnss;
+
+import java.sql.*;
+
+class Database {
+    private String database = "cnss-app";
+    private String username = "postgres";
+    private String password = "admin";
+    Connection cnx = null;
+    private Statement stmt = null;
+
+    public Database() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            cnx = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+database, username, password);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened connexion successfully");
+
+    }
+
+    public void query(String request){
+        try {
+            stmt = cnx.createStatement();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean execute(String rq) {
+        this.query(rq);
+        try {
+            stmt.executeUpdate(rq);
+            return true;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public void closeCnx(){
+        try {
+            stmt.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- test-app/src/main/java/org/cnss/Main.java
+++ test-app/src/main/java/org/cnss/Main.java
@@ -1,9 +1,28 @@
 package org.cnss;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        ConnectDB cn = new ConnectDB("cnss-app", "postgres", "admin");
-        System.out.println(cn.testConnection());
+        Database cn = new Database();
+        Scanner in = new Scanner(System.in);
+//      System.out.println(cn.testConnection());
 
+        List<String> ns = Arrays.asList("Adam","Omar","Aymen","Sofia");
+        List<String> square = ns.stream().map(el -> "Hello\t"+el).collect(Collectors.toList());
+        System.out.println("enter the name >> ");
+        String name = in.nextLine();
+        System.out.println("enter the address >> ");
+        String address = in.next();
+        System.out.println("enter the age >> ");
+        int age = in.nextInt();
+
+
+        // create table in db
+        String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (1, '"+ name +"', "+ age +", '"+ address +"', 20000.00 );";
+        if(cn.execute(sql)){
+            System.out.println("add success");
+        }
     }
 }
